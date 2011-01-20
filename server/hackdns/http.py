2a7e5b3262bbab3d0bb1c21013018a137b5b4fad
maze <maze@pyth0n.org>
2011-01-20 12:41:53 +0100

    Added HTTP basic functions

---
+++ server/hackdns/http.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+try:
+    import json
+except ImportError:
+    from django.utils import simplejson as json
+
+def Response(request, template, **data):
+    return render_to_response(template, data,
+        context_instance=RequestContext(request))
+
+def JsonResponse(**data):
+    serialized = json.dumps(data)
+    return HttpResponse(serialized,
+        content_type='text/plain') # FIXME: application/json
+
